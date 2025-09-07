c93e987f192fa77a6b462584f8a81a0661a08790
User <nindzyakay@gmail.com>
2025-09-07 17:27:40 +0300

    edgsegseg

--- less1.py
+++ less1.py
@@ -1,4 +1,30 @@
-print("Hello world!")
+# class Dog:
+#     def data(self, name, sex):
+#         self.age = 7
+#         print(name, "Gav", self.age, sex)
+#
+#
+# dog1 = Dog()
+# dog1.data("maksim", "trans")
+# dog2 = Dog()
+# dog2.data("archi", "devka")
 
-i = 1
-print(i)
+
+class Dog:
+    def __init__(self, name, sex):
+        self.age = 7
+        print(name, "Gav", self.age, sex)
+
+
+dog1 = Dog("Pika", "male")
+dog2 = Dog("Nester", "female")
+
+
+
+
+
+
+# def data(name, age=6):
+#     print(name, "Gav", age)
+
+# a = data("pedic")
